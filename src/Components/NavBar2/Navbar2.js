b545b43dfa637bd1bbda15947bf57973b1f54671
Gull-Sher <mobilegull73@gmail.com>
2023-05-12 09:58:10 +0500

    Deleting Extras

--- src/Components/NavBar2/Navbar2.js
+++ src/Components/NavBar2/Navbar2.js
@@ -72,3 +72,56 @@ function Navbar2() {
 }
 
 export default Navbar2;
+
+// ==================== Extra Codes of NavBars =============Blow this line===
+// import { ReactComponent as Brand } from '../../Assets/icons/logo.svg'
+// import { ReactComponent as Brand } from '../Assets/Icons/waheedgroup-logo1.jpg'
+// import '../Navbar/Navbar.css'
+
+// const Navbar = () => {
+//    return (
+
+//       <nav>
+//          <div class="logo">
+//             Waheed Group
+//          </div>
+//          <input type="checkbox" id="click" />
+//          <label for="click" class="menu-btn">
+//             <i class="fas fa-bars"></i>
+//          </label>
+//          <ul>
+//             <NavLink to="/">Home</NavLink> – The route to the home page is http://localhost:3000
+//             <NavLink to="/about">About</NavLink> – About page route that is, http://localhost:3000/about
+
+
+// {/* <NavLink to="/services">Services</NavLink> – Services page route that is, http://localhost:3000/services */}
+// {/* <NavLink to="/feedback">FeedBack</NavLink> – FeedBack page route that is, http://localhost:3000/feedback */}
+// {/* <NavLink to="/contact">ContactUs</NavLink> – ContactUs to the contact page that is http://localhost:3000/contactus */}
+// {/* <NavLink to="/blog">Blog</NavLink> – Route to the blog page that is http://localhost:3000/blog */}
+// {/* <NavLink to="/Gallery">Gallery</NavLink> – Route to the Gallery page that is http://localhost:3000/gallery */}
+
+
+// <li><a class="active" href="#">Home</a></li>
+// <li><a href="#">About</a></li>
+// <li><a href="#">Services</a></li>
+// <li><a href="#">Gallery</a></li>
+// <li><a href="#">Feedback</a></li>
+// <li><a href="#">ContactUs</a></li>
+
+
+//          </ul>
+//       </nav >
+
+//    )
+// }
+
+// export default Navbar
+
+
+//
+
+// import { ReactComponent as Brand } from '../../Assets/icons/logo.svg'
+// import { ReactComponent as Brand } from '../Assets/Icons/waheedgroup-logo1.jpg'
+// import '../Navbar/Navbar.css'
+
+
